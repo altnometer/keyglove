ab874b57a54ad0bdd361d544612ed5eaf16343b3
puppy <puppy@dog>
2019-05-21 18:12:01 +0300

    moved mdl0_switch_center away from center by 1.5 mm

--- fingers.py
+++ fingers.py
@@ -258,7 +258,7 @@ mdl0_length = tocm(22)
 mdl0_hight = tocm(11)
 
 # switch center
-mdl0_switch_center = (tocm(1.25), tocm(0), tocm(0))
+mdl0_switch_center = (tocm(1.25), tocm(1.5), tocm(0))
 
 # switch surface:
 # the incline magnitude
